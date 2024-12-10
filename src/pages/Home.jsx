a15712d54d1d2bf119f9c14fb11bf26419798635
Insomnia23-21 <danielmedinageek@gmail.com>
2024-12-10 11:38:15 -0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/gallery");
+  };
+
   return (
     <div
       className="hero-section d-flex align-items-center justify-content-center text-white"
@@ -9,20 +13,18 @@ const Home = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "100vh",
-        weight: "100vh",
         margin: 0,
       }}
     >
       <div className="hero-content text-center">
         <h1 className="display-4">¡Bienvenido a La Vitrina!</h1>
         <p className="lead">Encuentra lo que necesitas al mejor precio.</p>
-        <Link to="/gallery" className="btn btn-primary btn-lg">
+        <button className="btn btn-primary btn-lg" onClick={handleNavigate}>
           Explorar productos
-        </Link>
+        </button>
       </div>
     </div>
   );
 };
 
 export default Home;
-
